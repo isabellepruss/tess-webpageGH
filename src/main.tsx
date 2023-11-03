6ce4dd304d4258913c848aa6a0bbeb07c1ffbefc
isabellepruss <igpkc5@umsystem.edu>
2023-11-03 14:49:19 -0500

    change to main

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { ChakraProvider } from "@chakra-ui/react";
-import App from "./App";
 import { RouterProvider } from "react-router-dom";
 import "./index.css";
 import router from "./routes";
